7b025e91082e53a62a5f34599558eb35a316624e
rahulr6 <rahulr6@KTRAININD.COM>
2023-11-30 18:16:47 +0530

    adding sample code for testing applicatino

---
+++ calculator.py
@@ -0,0 +1,46 @@
+calc_code = """def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Cannot divide by zero"
+
+while True:
+    # User input for the operation
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    # Check if the user wants to exit
+    if choice == '5':
+        print("Calculator exiting. Goodbye!")
+        break
+
+    # Input for numbers
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    # Perform the selected operation
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid Input. Please enter a valid choice.")"""
